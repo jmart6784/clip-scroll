e6f6059c4e3acc21e22922754aa7b1a783379631
Juan <50716045+jmart6784@users.noreply.github.com>
2023-02-19 18:57:21 -0800

    Added homepage controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+
+    end
+  end
+
+  root 'homepage#index'
+  get '/*path' => 'homepage#index'
+end
 end
