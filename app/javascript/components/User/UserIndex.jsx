85938de4584e211180b34f604b33d322069cb4a8
Juan <50716045+jmart6784@users.noreply.github.com>
2023-02-19 22:18:30 -0800

    Added user show page logic

--- app/javascript/components/User/UserIndex.jsx
+++ app/javascript/components/User/UserIndex.jsx
@@ -2,7 +2,28 @@ import React, { useEffect, useState, useMemo } from "react";
 import { Link } from "react-router-dom";
 
 const UserIndex = () => { 
-  return <h1>User Index</h1>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/v1/users/index")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then((response) => setUsers(response))
+      .catch(() => console.log("Error getting users index"));
+  }, []);
+
+  let usersJsx = users.map(user => <div key={user.id}><Link to={`/users/show/${user.id}`}>{user.email}</Link></div>);
+
+  return (
+    <div>
+      <h1>User Index</h1>
+      {usersJsx}
+    </div>
+  );
 }
 
 export default UserIndex;
