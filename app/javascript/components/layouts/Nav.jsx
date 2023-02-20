b06b93c091b0ae261d4e0a7a285412ffaee26738
Juan <50716045+jmart6784@users.noreply.github.com>
2023-02-19 21:57:37 -0800

    Added condition render to nav bar

--- app/javascript/components/layouts/Nav.jsx
+++ app/javascript/components/layouts/Nav.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import UserContext from "../context/UserContext";
 
 const Nav = () => {
-  return (
-    <div>
-      <Link to="/">Home</Link>
-      <a href="/users/sign_in">Sign In</a>
-      <a href="/users/sign_up">Sign Up</a>
-    </div>
-  );
+  const [user, setUser] = useContext(UserContext);
+  
+  if (user.id) {
+    return (
+      <div>
+        <Link to="/">Home</Link>
+        <a rel="nofollow" data-method="delete" href="/users/sign_out">Log out</a>
+      </div>
+    );
+  } else { 
+    return (
+      <div>
+        <Link to="/">Home</Link>
+        <a href="/users/sign_in">Sign In</a>
+        <a href="/users/sign_up">Sign Up</a>
+      </div>
+    );
+  }
 };
 
 export default Nav;
