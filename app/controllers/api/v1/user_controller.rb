823d22688eb0d284a9c935c9c28141bf0b8332bd
Juan <50716045+jmart6784@users.noreply.github.com>
2023-02-19 20:51:18 -0800

    Added user controller and routes

---
+++ app/controllers/api/v1/user_controller.rb
@@ -0,0 +1,28 @@
+class Api::V1::UserController < ApplicationController
+  def index
+    users = User.all.order(created_at: :desc)
+    render json: users, status: 200
+  end
+
+  def show
+    if user
+      render json: user
+    else
+      render json: {}, status: 404
+    end
+  end
+
+  def user_info
+    if user_signed_in?
+      render json: current_user
+    else
+      render json: {}, status: 401
+    end
+  end
+
+  private
+
+  def user
+    @user ||= User.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get '/users/index', to: 'users#index'
+      get '/users/show/:id', to: 'users#show'
+      get '/users/user_info', to: 'users#user_info'
+    end
+  end
   devise_for :users
   namespace :api do
     namespace :v1 do
