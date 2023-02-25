0111ada884e32974c06e1d78717d672cc981055d
Juan <50716045+jmart6784@users.noreply.github.com>
2023-02-24 23:57:14 -0800

    Configured youtube video to autoplay and hide controls

--- app/javascript/components/YouTube/YouTubeVideo.jsx
+++ app/javascript/components/YouTube/YouTubeVideo.jsx
@@ -1,24 +1,48 @@
 import React, { useEffect, useState } from "react";
 
 const YoutubeVideo = () => { 
-  const [video, setVideo] = useState({});
+  // const [video, setVideo] = useState({});
   
-  useEffect(() => { 
-    fetch(`/api/v1/youtube/video/XYAe15w39LQ`)
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw new Error("Network response was not ok.");
-      })
-      .then((response) => setVideo(response))
-      .catch(() => console.log("Failed to get video"));
-  }, []);
-
+  // useEffect(() => { 
+  //   fetch(`/api/v1/youtube/video/XYAe15w39LQ`)
+  //     .then((response) => {
+  //       if (response.ok) {
+  //         return response.json();
+  //       }
+  //       throw new Error("Network response was not ok.");
+  //     })
+  //     .then((response) => setVideo(response))
+  //     .catch(() => console.log("Failed to get video"));
+  // }, []);
+//https://youtube.com/embed/XYAe15w39LQ
   return (
     <div>
       <h1>Youtube Video</h1>
-      {video.items ? <iframe className='video' title='Youtube player' sandbox='allow-same-origin allow-forms allow-popups allow-scripts allow-presentation' src={`https://youtube.com/embed/${video.items[0].id}?autoplay=1&mute=1`} allowFullScreen></iframe> : "Loading..."}
+      {/* {{
+        video.items ?
+          <iframe
+            className="youtube-video"
+            allow="autoplay"
+            title='Youtube player'
+            height="500"
+            idth="300"
+            sandbox='allow-same-origin allow-forms allow-popups allow-scripts allow-presentation'
+            src={`https://youtube.com/embed/${video.items[0].id}?autoplay=1&loop=1&mute=1&controls=0&playlist=${video.items[0].id}`}
+            allowFullScreen
+          ></iframe>
+        : "Loading..."
+      }} */}
+
+      <iframe
+        className="youtube-video"
+        allow="autoplay"
+        title='Youtube player'
+        height="500"
+        width="300"
+        sandbox='allow-same-origin allow-forms allow-popups allow-scripts allow-presentation'
+        src={`https://youtube.com/embed/XYAe15w39LQ?autoplay=1&loop=1&mute=1&controls=0&playlist=XYAe15w39LQ`}
+        allowFullScreen
+      ></iframe>
     </div>
   );
 }
