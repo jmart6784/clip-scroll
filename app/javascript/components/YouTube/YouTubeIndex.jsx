4668e7a2143f899906e14e6f84235de2acc82f3b
Juan <50716045+jmart6784@users.noreply.github.com>
2023-02-27 19:58:51 -0800

    Added loop to render youtube videos

--- app/javascript/components/YouTube/YouTubeIndex.jsx
+++ app/javascript/components/YouTube/YouTubeIndex.jsx
@@ -3,6 +3,7 @@ import YoutubeVideo from "./YouTubeVideo";
 
 const YouTubeIndex = () => { 
   const [videos, setVideos] = useState([]);
+  const [index, setIndex] = useState(0);
 
   useEffect(() => { 
     fetch("/api/v1/youtube/videos")
@@ -16,12 +17,21 @@ const YouTubeIndex = () => {
       .catch(() => console.log("Error getting quiz index"));
   }, []);
 
-  useEffect(() => console.log(videos), [videos]);
+  // useEffect(() => console.log(videos), [videos]);
+
+  let videosJsx = videos.slice(0, index + 1).map(video => { 
+    // console.log("LOOP V: ", video);
+    return (
+      <div key={video.video_id}>
+        <YoutubeVideo id={video.video_id} data={video} />
+      </div>
+    );
+  });
 
   return (
     <div>
       <h1>YouTube Index</h1>
-      <YoutubeVideo id="XYAe15w39LQ" data={{}} />
+      <div>{videosJsx}</div>
     </div>
   );
 }
