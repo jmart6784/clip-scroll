3eff914b20a0b90a4ab942ede0c3483793c1ef81
Juan <50716045+jmart6784@users.noreply.github.com>
2023-02-27 19:09:24 -0800

    Added backend data to YouTube index component

--- app/javascript/components/YouTube/YouTubeIndex.jsx
+++ app/javascript/components/YouTube/YouTubeIndex.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import YoutubeVideo from "./YouTubeVideo";
 
 const YouTubeIndex = () => { 
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => { 
+    fetch("/api/v1/youtube/videos")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then((response) => setVideos(response))
+      .catch(() => console.log("Error getting quiz index"));
+  }, []);
+
+  useEffect(() => console.log(videos), [videos]);
+
   return (
     <div>
       <h1>YouTube Index</h1>
