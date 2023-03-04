9a74b7d4bb5afa53ce478ba129331d301347755f
Juan <50716045+jmart6784@users.noreply.github.com>
2023-03-03 21:48:10 -0800

    added search action

--- app/controllers/api/v1/youtube_controller.rb
+++ app/controllers/api/v1/youtube_controller.rb
@@ -16,6 +16,10 @@ class Api::V1::YoutubeController < ApplicationController
     render json: {"kind"=>"youtube#videoListResponse", "etag"=>"nkpHW4Q5v0_GR8mANaKzrX6_ZnU", "items"=>[{"kind"=>"youtube#video", "etag"=>"9Ko4pciJptfkvBz4OdfY9z-XQHU", "id"=>"XYAe15w39LQ", "snippet"=>{"publishedAt"=>"2023-02-24T21:56:36Z", "channelId"=>"UCdC0An4ZPNr_YiFiYoVbwaw", "title"=>"Watermelon without the melon", "description"=>"Source: Collab.Inc\n\n#shorts", "thumbnails"=>{"default"=>{"url"=>"https://i.ytimg.com/vi/XYAe15w39LQ/default.jpg", "width"=>120, "height"=>90}, "medium"=>{"url"=>"https://i.ytimg.com/vi/XYAe15w39LQ/mqdefault.jpg", "width"=>320, "height"=>180}, "high"=>{"url"=>"https://i.ytimg.com/vi/XYAe15w39LQ/hqdefault.jpg", "width"=>480, "height"=>360}, "standard"=>{"url"=>"https://i.ytimg.com/vi/XYAe15w39LQ/sddefault.jpg", "width"=>640, "height"=>480}, "maxres"=>{"url"=>"https://i.ytimg.com/vi/XYAe15w39LQ/maxresdefault.jpg", "width"=>1280, "height"=>720}}, "channelTitle"=>"Daily Dose Of Internet", "tags"=>["Internet", "Internet videos", "Daily Dose", "Animals", "TikTok", "Science", "TikTok Compilation", "Compilation", "Daily Dose Of Internet", "watermelon"], "categoryId"=>"24", "liveBroadcastContent"=>"none", "defaultLanguage"=>"en", "localized"=>{"title"=>"Watermelon without the melon", "description"=>"Source: Collab.Inc\n\n#shorts"}, "defaultAudioLanguage"=>"en"}, "statistics"=>{"viewCount"=>"184328", "likeCount"=>"7034", "favoriteCount"=>"0", "commentCount"=>"410"}}], "pageInfo"=>{"totalResults"=>1, "resultsPerPage"=>1}}
   end
 
+  def search
+        
+  end
+
   def comments
     # response = HTTParty.get("https://www.googleapis.com/youtube/v3/commentThreads?part=snippet,id,replies&videoId=#{params[:video_id]}&maxResults=100&order=relevance&textFormat=plainText&key=#{Rails.application.credentials.dig(:youtube_api_key)}")
 
