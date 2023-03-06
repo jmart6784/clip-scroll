0f5ac0fe7254577291f88ebbda220acb275ea54e
Juan <50716045+jmart6784@users.noreply.github.com>
2023-03-05 23:52:55 -0800

    Rendered channel data

--- app/javascript/components/YouTube/YouTubeChannels.jsx
+++ app/javascript/components/YouTube/YouTubeChannels.jsx
@@ -17,9 +17,29 @@ const YouTubeChannels = () => {
 
   useEffect(() => console.log(channels), [channels]);
 
+  let channelsJsx = channels.map(c => { 
+    let channel = c.items[0].snippet;
+    let stats = c.items[0].statistics;
+    let channelId = c.items[0].id;
+
+    return (
+      <div key={channelId}>
+        <a href={`https://www.youtube.com/${channel.customUrl}`} target="_blank">
+          <img src={channel.thumbnails.default.url} alt="channel avatar" />
+          <span>{channel.title}</span>
+        </a>
+        <p>Description: {channel.description}</p>
+        <p>Subscribers: {stats.subscribersCount}</p>
+        <p>Video Count: {stats.videoCount}</p>
+        <p>Total Views Count: {stats.viewCount}</p>
+      </div>
+    );
+  });
+
   return (
     <div>
       <h1>YouTube Channels</h1>
+      {channelsJsx}
     </div>
   );
 }
