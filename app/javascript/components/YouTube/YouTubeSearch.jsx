3e12f57574acf139054b776afb2cf9dd18ca3b19
Juan <50716045+jmart6784@users.noreply.github.com>
2023-03-05 19:19:40 -0800

    created addshorts() function

--- app/javascript/components/YouTube/YouTubeSearch.jsx
+++ app/javascript/components/YouTube/YouTubeSearch.jsx
@@ -4,7 +4,11 @@ const YouTubeSearch = () => {
   const [results, setResults] = useState({items: []});
   const [search, setSearch] = useState("Yes Theory");
 
-  const Search = () => {
+  const addShorts = (channelId) => { 
+    console.log("Add shorts", channelId);
+  }
+
+  const searchChannels = () => {
     fetch(`/api/v1/youtube/search/${search}`)
       .then((response) => {
         if (response.ok) {
@@ -19,10 +23,12 @@ const YouTubeSearch = () => {
   useEffect(() => console.log(results), [results]);
 
   let resultsJsx = results.items.map(result => {
+    let channel = result.snippet;
     return (
-      <div key={result.snippet.channelId}>
-        <img src={result.snippet.thumbnails.default.url} alt="channel avatar" />
-        <span>{result.snippet.title}</span>
+      <div key={channel.channelId}>
+        <img src={channel.thumbnails.default.url} alt="channel avatar" />
+        <span>{channel.title}</span>
+        <button onClick={() => addShorts(channel.channelId)}>Add Shorts</button>
       </div>
     );
   });
@@ -31,7 +37,7 @@ const YouTubeSearch = () => {
     <div>
       <h1>YouTube Search</h1>
       <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
-      <button onClick={Search} disabled={search.trim() == "" ? true : false}>Search</button>
+      <button onClick={searchChannels} disabled={search.trim() == ""}>Search</button>
 
       {resultsJsx}
     </div>
