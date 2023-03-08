6f871abca315ae21ec1f9c9e4e7fddadbf7ec532
Juan <50716045+jmart6784@users.noreply.github.com>
2023-03-07 23:50:58 -0800

    Changed column type from string to text on Youtube channel model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_08_054215) do
+ActiveRecord::Schema.define(version: 2023_03_08_074900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,7 +73,7 @@ ActiveRecord::Schema.define(version: 2023_03_08_054215) do
   end
 
   create_table "youtube_channels", force: :cascade do |t|
-    t.string "name"
+    t.text "name"
     t.text "channel_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
