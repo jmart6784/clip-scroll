c0ce65c7e3290316d64d93bb59d7a0159a013a49
Juan <50716045+jmart6784@users.noreply.github.com>
2023-03-10 22:27:35 -0800

    Added index, create, show, destroy actions to Playlist controller

--- app/controllers/api/v1/playlists_controller.rb
+++ app/controllers/api/v1/playlists_controller.rb
@@ -1,2 +1,38 @@
 class Api::V1::PlaylistsController < ApplicationController
+  def index
+    playlist = Playlist.all.order(created_at: :desc)
+    render json: playlist
+  end
+
+  def create
+    playlist = Playlist.create!(playlist_params)
+    if playlist
+      render json: playlist
+    else
+      render json: playlist.errors
+    end
+  end
+
+  def show
+    if playlist
+      render json: playlist
+    else
+      render json: playlist.errors
+    end
+  end
+
+  def destroy
+    playlist&.destroy
+    render json: { message: 'playlist deleted!' }
+  end
+
+  private
+
+  def playlist_params
+    params.permit(:name, :private, :source)
+  end
+
+  def playlist
+    @playlist ||= Playlist.find(params[:id])
+  end
 end
