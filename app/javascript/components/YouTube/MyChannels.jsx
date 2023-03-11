542afe6f95fa4098863489c20b5016ea577eb789
Juan <50716045+jmart6784@users.noreply.github.com>
2023-03-10 20:21:23 -0800

    Added request to backend for my_channels data

--- app/javascript/components/YouTube/MyChannels.jsx
+++ app/javascript/components/YouTube/MyChannels.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const MyChannels = () => { 
+  const [channels, setChannels] = useState([]);
+
+  useEffect(() => { 
+    fetch(`/api/v1/youtube/my_channels`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then((response) => setChannels(response))
+      .catch(() => console.log("Error getting data"));
+  }, []);
+
+  useEffect(() => console.log(channels), [channels]);
+  
   return (
     <div>
       <h1>My Channels</h1>
