93b14bcd4792b11744790375ff425fbafa7cd356
Juan <50716045+jmart6784@users.noreply.github.com>
2023-03-10 21:09:48 -0800

    Added render logic for ChannelTile in MyChannels component

--- app/javascript/components/YouTube/MyChannels.jsx
+++ app/javascript/components/YouTube/MyChannels.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import ChannelTile from "./ChannelTile";
 
 const MyChannels = () => { 
   const [channels, setChannels] = useState([]);
+  const [addedChannels, setAddedChannels] = useState([]);
 
   useEffect(() => { 
     fetch(`/api/v1/youtube/my_channels`)
@@ -14,7 +16,75 @@ const MyChannels = () => {
       })
       .then((response) => setChannels(response))
       .catch(() => console.log("Error getting data"));
+    
+    fetch(`/api/v1/youtube/added_channels`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then((response) => setAddedChannels(response))
+      .catch(() => console.log("Error getting data"));
   }, []);
+
+  const addShorts = (e, channelId) => {
+    fetch(`/api/v1/youtube/add_shorts/${channelId}`, {
+      method: "POST",
+      headers: {
+        "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]').content,
+      }
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then((response) => {
+        if (typeof response == "object") {
+          e.target.disabled = true;
+          if (response.length == 0) {
+            e.target.textContent = "No shorts found";
+          } else { 
+            e.target.textContent = "Added";
+          }
+        }
+      })
+      .catch(() => console.log("Error getting shorts data"));
+  }
+
+  const removeShorts = (e, channelId) => { 
+    fetch(`/api/v1/youtube/remove_shorts/${channelId}`, {
+      method: "DELETE",
+      headers: {
+        "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]').content,
+      }
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then((response) => {
+        if (response.message === 'Channel removed') {
+          e.target.disabled = true;
+          e.target.textContent = "Removed";
+        }
+      })
+      .catch(() => console.log("Error deleting shorts data"));
+  }
+
+  let channelsJsx = channels.map(c =>
+    <ChannelTile
+      key={c.items[0].id}
+      channel={c}
+      addShorts={addShorts}
+      removeShorts={removeShorts}
+      addedChannels={addedChannels} 
+    />
+  );
 
   useEffect(() => console.log(channels), [channels]);
   
@@ -22,6 +92,7 @@ const MyChannels = () => {
     <div>
       <Link to="/youtube/channels">More Channels</Link>
       <h1>My Channels</h1>
+      {channelsJsx}
     </div>
   );
 }
