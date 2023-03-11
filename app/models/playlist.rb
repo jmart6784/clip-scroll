20acf9231fd1da3e420035e60ba6ff1b37f0fcaf
Juan <50716045+jmart6784@users.noreply.github.com>
2023-03-10 22:13:16 -0800

    Added playlist length validtion for name

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,6 +1,8 @@
 class Playlist < ApplicationRecord
   belongs_to :user
+  
   validates :name, :source, :user_id, presence: true
   validates :private, inclusion: { in: [ true, false ] }
   validates :source, inclusion: { in: [ "youtube", "reddit", "mix" ] }
+  validates :name, length: {minimum: 1, maximum: 50}
 end
