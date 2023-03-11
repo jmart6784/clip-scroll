3296c759bbd30c0b6cabdabd3340487ef2c3ae09
Juan <50716045+jmart6784@users.noreply.github.com>
2023-03-10 23:14:52 -0800

    added playlist data to show component

--- app/javascript/components/Playlist/PlaylistShow.jsx
+++ app/javascript/components/Playlist/PlaylistShow.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const PlaylistShow = () => { 
+const PlaylistShow = (props) => { 
+  const [playlist, setPlaylist] = useState({
+    id: 0,
+    name: '',
+    source: 'youtube',
+    private: false,
+    user_id: 0,
+    created_at: '',
+    updated_at: ''
+  });
+
+  useEffect(() => { 
+    fetch(`/api/v1/playlist/show/${props.match.params.id}`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then((response) => setPlaylist(response))
+      .catch(() => console.log("Error getting data"));
+  }, []);
+
   return (
     <div>
       <h1>Playlist Show</h1>
+      <p>name: {playlist.name}</p>
+      <p>Private: {playlist.private.toString()}</p>
+      <p>Source: {playlist.source}</p>
     </div>
   );
 }
