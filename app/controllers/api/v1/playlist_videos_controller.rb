6441af099f6070efb5b53210bd5a051db6ac89f3
Juan <50716045+jmart6784@users.noreply.github.com>
2023-03-12 00:04:13 -0800

    Added api request to playlist videos

--- app/controllers/api/v1/playlist_videos_controller.rb
+++ app/controllers/api/v1/playlist_videos_controller.rb
@@ -1,6 +1,16 @@
 class Api::V1::PlaylistVideosController < ApplicationController
+  include HTTParty
+
   def videos
-    videos = PlaylistVideo.where(playlist_id: params[:playlist_id])
+    videos = []
+
+    PlaylistVideo.where(playlist_id: params[:playlist_id]).each do |pv|
+      if pv.source === "youtube"
+        response = HTTParty.get("https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&id=#{pv.video_id}&key=#{Rails.application.credentials.dig(:youtube_api_key)}")
+
+        videos << JSON.parse(response.body)
+      end
+    end
 
     render json: videos
   end
