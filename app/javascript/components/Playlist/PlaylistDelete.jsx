c5aa1d60c9cde02f236b15dc17dcffdc6716d58a
Juan <50716045+jmart6784@users.noreply.github.com>
2023-03-11 21:01:00 -0800

    Added delete for playlist

--- app/javascript/components/Playlist/PlaylistDelete.jsx
+++ app/javascript/components/Playlist/PlaylistDelete.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 
-const PlaylistDelete = () => {
+const PlaylistDelete = (props) => {
+  const deletePlaylist = () => { 
+    fetch(`/api/v1/playlist/destroy/${props.id}`, {
+      method: "DELETE",
+      headers: {
+        "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]').content,
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then((response) => props.history.push(`/playlist/index`))
+      .catch((error) => console.log(error.message));
+  }
 
   return (
     <div>
-      <h1>Playlist Delete</h1>
+      <button onClick={deletePlaylist} type="button">Delete</button>
     </div>
   );
 };
