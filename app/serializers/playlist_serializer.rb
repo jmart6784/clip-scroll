459626270b75ec350c1e39665fc23fdbd4cee0b6
Juan <50716045+jmart6784@users.noreply.github.com>
2023-03-11 22:57:10 -0800

    Added playlist videos association to serializer

--- app/serializers/playlist_serializer.rb
+++ app/serializers/playlist_serializer.rb
@@ -1,4 +1,5 @@
 class PlaylistSerializer < ActiveModel::Serializer
   attributes :id, :name, :source, :private, :user_id, :created_at, :updated_at
   belongs_to :user
+  has_many :playlist_videos
 end
