11c6a2f5177107593c7ca28c6d46949eb0ca7eeb
Juan <50716045+jmart6784@users.noreply.github.com>
2023-03-11 22:41:47 -0800

    Association for playlist video in playlist model

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,5 +1,6 @@
 class Playlist < ApplicationRecord
   belongs_to :user
+  has_many :playlist_videos, dependent: :destroy
   
   validates :name, :source, :user_id, presence: true
   validates :private, inclusion: { in: [ true, false ] }
