3815ba5f89f5c6f9bc2ba361bc29d4eafd6361d7
Juan <50716045+jmart6784@users.noreply.github.com>
2023-03-11 22:36:48 -0800

    Added play list video model validations

--- app/models/playlist_video.rb
+++ app/models/playlist_video.rb
@@ -1,4 +1,7 @@
 class PlaylistVideo < ApplicationRecord
   belongs_to :user
   belongs_to :playlist
+
+  validates :source, :playlist_id, :video_id, :parent_source_id, :user_id, presence: true
+  validates :source, inclusion: { in: [ "youtube", "reddit", "mix" ] }
 end
