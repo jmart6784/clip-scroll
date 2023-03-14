17a17095df732b10476912d48013bbd81f5e1c3c
Juan <50716045+jmart6784@users.noreply.github.com>
2023-03-13 20:07:38 -0700

    Added backend data to Playlist video component

--- app/javascript/components/Playlist_video/PlaylistVideoMenu.jsx
+++ app/javascript/components/Playlist_video/PlaylistVideoMenu.jsx
@@ -1,7 +1,22 @@
 import React, { useEffect, useState } from "react";
 
 const PlaylistVideoMenu = () => { 
+  const [playlists, setPlaylists] = useState([]);
   const [prompt, setPrompt] = useState(false);
+
+  useEffect(() => { 
+    fetch("/api/v1/playlist/mine")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then((response) => setPlaylists(response))
+      .catch(() => console.log("Error getting playlist data"));
+  }, []);
+
+  useEffect(() => console.log(playlists), [playlists]);
 
   let menuJsx = <button type="button" onClick={() => setPrompt(true)}>Prompt</button>;
 
