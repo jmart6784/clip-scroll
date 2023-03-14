74fdc4732b64cfe2c8990fe7fe734db53e07c478
Juan <50716045+jmart6784@users.noreply.github.com>
2023-03-13 22:50:35 -0700

    Added functionality to toggle checkboxes

--- app/javascript/components/Playlist_video/PlaylistVideoMenu.jsx
+++ app/javascript/components/Playlist_video/PlaylistVideoMenu.jsx
@@ -5,7 +5,7 @@ const PlaylistVideoMenu = (props) => {
   const [prompt, setPrompt] = useState(false);
 
   useEffect(() => { 
-    fetch(`/api/v1/playlist/mine?video_id=${props.videoId}`)
+    fetch(`/api/v1/playlist/mine?video_id=XYAe15w39LQ`)
       .then((response) => {
         if (response.ok) {
           return response.json();
@@ -16,16 +16,26 @@ const PlaylistVideoMenu = (props) => {
       .catch(() => console.log("Error getting playlist data"));
   }, [props]);
 
-  useEffect(() => console.log(playlists), [playlists]);
+  // useEffect(() => console.log(playlists), [playlists]);
 
-  const onCheck = (event) => { 
-    const { name, checked } = event.target;
-    setForms({...forms, [name]: checked});
+  const onCheck = (videoId, playlistId) => { 
+    let pls = playlists
+    let indexOfMatch = pls.findIndex(pl => pl.id === playlistId);
+
+    // If there is a match by playlist ID toggle checkbox and added value.
+    if (indexOfMatch != -1) {
+      pls[indexOfMatch]["added"] = !pls[indexOfMatch]["added"];
+    } else { 
+      console.log("Playlist not found");
+    }
+
+    setPlaylists([...pls ]);
+    // console.log(videoId, playlistId);
   }
 
   let playlistJsx = playlists.map(pl =>
     <div key={pl.id}>
-      <input name={pl.id} type="checkbox" checked={pl.added} onChange={onCheck} />
+      <input type="checkbox" checked={pl.added} onClick={() => onCheck(props.videoId, pl.id)} />
       <p>{pl.name}</p>
       <p>Private {pl.private.toString()}</p>
       <i>source: {pl.source}</i>
