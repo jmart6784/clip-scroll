7516f254bcd6c5dcb9d85b90b75f405f48542233
Juan <50716045+jmart6784@users.noreply.github.com>
2023-03-19 16:35:52 -0700

    Added filter to data to remove non-vertical videos

--- app/javascript/components/Reddit/RedditIndex.jsx
+++ app/javascript/components/Reddit/RedditIndex.jsx
@@ -56,23 +56,27 @@ const RedditIndex = () => {
   if (posts['data']) {
     videosJsx = posts['data']['children'].map(post => { 
       if (post['data']['media']) {
-        let postId = post['data']['id']
-        
-        let awards = 0;
+        let height = post['data']['media']['reddit_video']['height']; 
+        let width = post['data']['media']['reddit_video']['width']; 
+        if (height > width) {
+          let postId = post['data']['id']
+          
+          let awards = 0;
 
-        post['data']['all_awardings'].forEach(award => awards += award['count']);
+          post['data']['all_awardings'].forEach(award => awards += award['count']);
 
-        return (
-          <div key={postId}>
-            <p>{postId}</p>
-            <p>{post['data']['subreddit_name_prefixed']}</p>
-            <p>{post['data']['title']}</p>
-            <p>{post['data']['score']}</p>
-            <p>Posted by {post['data']['author']}</p>
-            <p>All awardings: {awards}</p>
-            <video src={post['data']['media']['reddit_video']['fallback_url']} width="300" height="500" controls></video>
-          </div>
-        ); 
+          return (
+            <div key={postId}>
+              <p>{postId}</p>
+              <p>{post['data']['subreddit_name_prefixed']}</p>
+              <p>{post['data']['title']}</p>
+              <p>{post['data']['score']}</p>
+              <p>Posted by {post['data']['author']}</p>
+              <p>All awardings: {awards}</p>
+              <video src={post['data']['media']['reddit_video']['fallback_url']} width="300" height="500" controls></video>
+            </div>
+          );  
+        }
       }
     });
   }
