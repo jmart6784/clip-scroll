c73afd658dc5c47cb1219178ff1eea8bb540be07
Juan <50716045+jmart6784@users.noreply.github.com>
2023-03-19 19:42:45 -0700

    Added audio tag to reddit video

--- app/javascript/components/Reddit/RedditVideo.jsx
+++ app/javascript/components/Reddit/RedditVideo.jsx
@@ -4,6 +4,8 @@ const RedditVideo = (props) => {
   let post = props.post;
   let postId = post['data']['id']
   let awards = 0;
+  let audioUrl = post['data']['media']['reddit_video']['scrubber_media_url'];
+  audioUrl = audioUrl.replace(/DASH_.*?.mp4/, 'DASH_audio.mp4');
 
   post['data']['all_awardings'].forEach(award => awards += award['count']);
 
@@ -16,7 +18,8 @@ const RedditVideo = (props) => {
       <p>Posted by {post['data']['author']}</p>
       <p>All awardings: {awards}</p>
       <p>Comments: {post['data']['num_comments']}</p>
-      <video src={post['data']['media']['reddit_video']['fallback_url']} width="300" height="500" controls></video>
+      <video src={post['data']['media']['reddit_video']['fallback_url']} width="300" height="500" controls autoPlay></video>
+      <audio src={audioUrl} controls autoPlay/>
     </div>
   );
 }
