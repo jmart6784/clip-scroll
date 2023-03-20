9a846cafd2977a31465ac7c789bbd3d00153b9ef
Juan <50716045+jmart6784@users.noreply.github.com>
2023-03-19 18:31:45 -0700

    Added RedditVideo.jsx component logic

--- app/javascript/components/Reddit/RedditIndex.jsx
+++ app/javascript/components/Reddit/RedditIndex.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import RedditVideo from "./RedditVideo";
 
 const RedditIndex = () => {
   const [posts, setPosts] = useState({});
@@ -57,28 +58,7 @@ const RedditIndex = () => {
   if (posts['data']) {
     videosJsx = posts['data']['children'].map(post => { 
       if (post['data']['media']) {
-        let height = post['data']['media']['reddit_video']['height']; 
-        let width = post['data']['media']['reddit_video']['width']; 
-        if (height > width) {
-          let postId = post['data']['id']
-          
-          let awards = 0;
-
-          post['data']['all_awardings'].forEach(award => awards += award['count']);
-
-          return (
-            <div key={postId}>
-              <p>{postId}</p>
-              <p>{post['data']['subreddit_name_prefixed']}</p>
-              <p>{post['data']['title']}</p>
-              <p>{post['data']['score']}</p>
-              <p>Posted by {post['data']['author']}</p>
-              <p>All awardings: {awards}</p>
-              <p>Comments: {post['data']['num_comments']}</p>
-              <video src={post['data']['media']['reddit_video']['fallback_url']} width="300" height="500" controls></video>
-            </div>
-          );  
-        }
+        return <RedditVideo key={post['data']['id']} post={post} />;
       }
     });
   }
--- app/javascript/components/Reddit/RedditVideo.jsx
+++ app/javascript/components/Reddit/RedditVideo.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 
-const RedditVideo = () => { 
-  return (
-    <div>
-      <h1>Reddit Video</h1>
-    </div>
-  );
+const RedditVideo = (props) => {
+  let post = props.post;
+  let height = post['data']['media']['reddit_video']['height']; 
+  let width = post['data']['media']['reddit_video']['width']; 
+
+  if (height > width) {
+    let postId = post['data']['id']
+    
+    let awards = 0;
+
+    post['data']['all_awardings'].forEach(award => awards += award['count']);
+
+    return (
+      <div key={postId}>
+        <p>{postId}</p>
+        <p>{post['data']['subreddit_name_prefixed']}</p>
+        <p>{post['data']['title']}</p>
+        <p>{post['data']['score']}</p>
+        <p>Posted by {post['data']['author']}</p>
+        <p>All awardings: {awards}</p>
+        <p>Comments: {post['data']['num_comments']}</p>
+        <video src={post['data']['media']['reddit_video']['fallback_url']} width="300" height="500" controls></video>
+      </div>
+    );
+  } else { return <div></div> }
 }
 
 export default RedditVideo;
