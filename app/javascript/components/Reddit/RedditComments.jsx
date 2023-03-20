13aec2ed8f2e18064527a87c907093a307696cff
Juan <50716045+jmart6784@users.noreply.github.com>
2023-03-19 21:02:51 -0700

    Added Comments logic to components

--- app/javascript/components/Reddit/RedditComments.jsx
+++ app/javascript/components/Reddit/RedditComments.jsx
@@ -1,6 +1,23 @@
 import React from "react";
 
 const RedditComments = () => { 
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    console.log(props.post['data']['id']);
+    fetch(`https://www.reddit.com/r/interestingasfuck/comments/${props.post['data']['id']}.json?raw_json=1`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then((response) => setComments(response))
+      .catch(() => console.log("Error getting data"));
+  }, [props]);
+
+  useEffect(() => console.log(comments), [comments]);
+  
   return (
     <div>
       <h1>Reddit Comments</h1>
--- app/javascript/components/Reddit/RedditVideo.jsx
+++ app/javascript/components/Reddit/RedditVideo.jsx
@@ -1,23 +1,6 @@
 import React, { useEffect, useState } from "react";
 
 const RedditVideo = (props) => {
-  const [comments, setComments] = useState([]);
-
-  useEffect(() => {
-    console.log(props.post['data']['id']);
-    fetch(`https://www.reddit.com/r/interestingasfuck/comments/${props.post['data']['id']}.json?raw_json=1`)
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw new Error("Network response was not ok.");
-      })
-      .then((response) => setComments(response))
-      .catch(() => console.log("Error getting data"));
-  }, [props]);
-
-  useEffect(() => console.log(comments), [comments]);
-
   let post = props.post;
   let postId = post['data']['id']
   let awards = 0;
