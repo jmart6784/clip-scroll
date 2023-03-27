deb27237ea8e4c8ab72a73a90c37ca04713248b7
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-03-26 21:11:30 -0700

    Created mine action for addedsubreddits

--- app/controllers/api/v1/added_subreddits_controller.rb
+++ app/controllers/api/v1/added_subreddits_controller.rb
@@ -1,2 +1,6 @@
 class Api::V1::AddedSubredditsController < ApplicationController
+  def mine
+    addedsubreddits = current_user.addedsubreddits.order('subreddit')
+    render json: addedsubreddits, status: 200
+  end
 end
