ec7f8574c22be1de96d3425e7b7c303abdcfb314
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-03-26 21:19:10 -0700

    Added mysubreddits component logic

--- app/javascript/components/Reddit/MySubreddits.jsx
+++ app/javascript/components/Reddit/MySubreddits.jsx
@@ -1,9 +1,34 @@
 import React from "react";
 
 const MySubreddits = () => { 
+  const [subreddits, setSubreddits] = useState([]);
+
+  useEffect(() => { 
+    fetch("/api/v1/addedsubreddit/mine")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then((response) => setSubreddits(response))
+      .catch(() => console.log("Error getting subreddit data"));
+  }, []);
+
+  let subredditsJsx = <div>Loading...</div>;
+
+  subredditsJsx = subreddits.map(subreddit => 
+    <div key={subreddit.id}>
+      <Link to={`/reddit/show/${subreddit.subreddit}`}>
+        <p>{subreddit.subreddit}</p>
+      </Link>
+    </div>  
+  );
+
   return (
     <div>
       <h1>My Subreddits</h1>
+      {subredditsJsx}
     </div>
   );
 }
