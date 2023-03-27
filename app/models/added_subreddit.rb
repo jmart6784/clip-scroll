2d6ae8f90ea5bb9fdded499be5642492b56ad3c0
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-03-26 21:03:09 -0700

    Created custom validation to check for duplicate subreddit additions

--- app/models/added_subreddit.rb
+++ app/models/added_subreddit.rb
@@ -1,4 +1,14 @@
 class AddedSubreddit < ApplicationRecord
   belongs_to :user
   validates :subreddit, :user_id, presence: true
+
+  validate :duplicate_subreddits
+
+  def duplicate_subreddits
+    existing_addition = AddedSubreddit.find_by(user_id: user_id, subreddit: subreddit)
+
+    unless existing_addition.nil?
+      errors.add(:subreddit, "has already been added")
+    end
+  end
 end
