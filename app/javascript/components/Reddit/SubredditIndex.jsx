07f79d060f3fa59e292027b8f4e341b898c89e30
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-03-26 18:42:37 -0700

    Added subreddit back end request

--- app/javascript/components/Reddit/SubredditIndex.jsx
+++ app/javascript/components/Reddit/SubredditIndex.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const SubredditIndex = () => { 
+  const [subreddits, setSubreddits] = useState([]);
+
+  useEffect(() => { 
+    fetch("/api/v1/subreddit/index")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then((response) => setSubreddits(response))
+      .catch(() => console.log("Error getting subreddit data"));
+  }, []);
+
+  useEffect(() => console.log(subreddits), [subreddits]);
+
   return (
     <div>
       <h1>Subreddit Index</h1>
