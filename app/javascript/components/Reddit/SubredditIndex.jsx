615eb9677ecdce4ef2e2be142d6c60c02b88e114
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-03-26 18:54:12 -0700

    Rendered subreddits in frontend

--- app/javascript/components/Reddit/SubredditIndex.jsx
+++ app/javascript/components/Reddit/SubredditIndex.jsx
@@ -15,11 +15,18 @@ const SubredditIndex = () => {
       .catch(() => console.log("Error getting subreddit data"));
   }, []);
 
-  useEffect(() => console.log(subreddits), [subreddits]);
+  let subredditsJsx = <div>Loading...</div>;
+
+  subredditsJsx = subreddits.map(subreddit => 
+    <div key={subreddit.id}>
+      <p>{subreddit.subreddit}</p>
+    </div>  
+  );
 
   return (
     <div>
       <h1>Subreddit Index</h1>
+      {subredditsJsx}
     </div>
   )
 }
