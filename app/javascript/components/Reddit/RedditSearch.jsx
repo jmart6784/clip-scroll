e06df8c7e422dac4d61ad41f4735674a78be199e
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-04-03 22:05:49 -0700

    Added Subreddit search API request and state for data

--- app/javascript/components/Reddit/RedditSearch.jsx
+++ app/javascript/components/Reddit/RedditSearch.jsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const RedditSearch = () => { 
+  const [search, setSearch] = useState("");
+  const [subreddits, setSubreddits] = useState([]);
+
+  useEffect(() => { 
+    fetch(`http://www.reddit.com/search.json?q=${search}&sort=relevance&type=sr&limit=100`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then((response) => setSubreddits(response['data']['children']))
+      .catch(() => console.log("Error getting subreddit data"));
+  }, []);
+
+  useEffect(() => console.log(subreddits), [subreddits]);
+
   return (
     <div>
       <h1>Reddit Search</h1>
