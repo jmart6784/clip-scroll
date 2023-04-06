c001aaf28e589b9323165f01f5a6a21fdbc0189c
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-04-05 21:22:09 -0700

    Conditionally rendered comments view

--- app/javascript/components/YouTube/YouTubeComments.jsx
+++ app/javascript/components/YouTube/YouTubeComments.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 const YouTubeComments = (props) => {
   const [comments, setComments] = useState({ items: [] });
   const [prompt, setPrompt] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   // Get comments and reply thread
   useEffect(() => {
@@ -14,67 +15,74 @@ const YouTubeComments = (props) => {
           }
           throw new Error("Network response was not ok.");
         })
-        .then((response) => setComments(response))
+        .then((response) => setComments(response), setLoading(false))
         .catch(() => console.log("Failed to get video"));
     }
   }, [props.id, prompt]);
 
-  // Generate logic to display comments and replies
-  let commentsJsx = comments.items.map(comment => { 
-    let parentComment = comment.snippet.topLevelComment.snippet;
-    let replies = comment.replies ? comment.replies.comments : [];
+  let menuJsx = <button type="button" onClick={() => setPrompt(true)}>Show Comments ({props.commentCount})</button>;
 
-    replies = replies.map(reply => { 
-      let r = reply.snippet;
+  let commentsJsx = <h1>...Loading</h1>;
+
+  // Generate logic to display comments and replies
+  if (loading === false && comments.items.length > 0) {
+    commentsJsx = comments.items.map(comment => { 
+      let parentComment = comment.snippet.topLevelComment.snippet;
+      let replies = comment.replies ? comment.replies.comments : [];
+
+      replies = replies.map(reply => { 
+        let r = reply.snippet;
+
+        return (
+          <div key={reply.id}>
+            <p>
+              <a href={r.authorChannelUrl} target="_blank">
+                <img src={r.authorProfileImageUrl} alt="user avatar" />
+              </a> {r.authorDisplayName}
+            </p>
+            <p>{r.textDisplay}</p>
+            <p>Likes: {r.likeCount}</p>
+          </div>
+        );
+      });
 
       return (
-        <div key={reply.id}>
+        <div key={comment.id}>
           <p>
-            <a href={r.authorChannelUrl} target="_blank">
-              <img src={r.authorProfileImageUrl} alt="user avatar" />
-            </a> {r.authorDisplayName}
+            <a href={parentComment.authorChannelUrl} target="_blank">
+              <img src={parentComment.authorProfileImageUrl} alt="user avatar" />
+            </a> {parentComment.authorDisplayName}
           </p>
-          <p>{r.textDisplay}</p>
-          <p>Likes: {r.likeCount}</p>
+          <p>{parentComment.textDisplay}</p>
+          <p>
+            Likes: {parentComment.likeCount}
+            {
+              // Hide replies if clicked
+              comment.replies ?
+                <button onClick={() => {
+                  let repliesContainer = document.getElementById(comment.snippet.topLevelComment.id)
+
+                  repliesContainer.style.display == "block" ?
+                    repliesContainer.style.display = "none"
+                  :
+                    repliesContainer.style.display = "block"
+                }}>
+                  {comment.replies.comments.length} replies
+                </button>
+              : ""
+            }
+          </p>
+          <div id={comment.snippet.topLevelComment.id} style={{display: "none"}}>
+            {replies}
+          </div>
         </div>
       );
     });
-
-    return (
-      <div key={comment.id}>
-        <p>
-          <a href={parentComment.authorChannelUrl} target="_blank">
-            <img src={parentComment.authorProfileImageUrl} alt="user avatar" />
-          </a> {parentComment.authorDisplayName}
-        </p>
-        <p>{parentComment.textDisplay}</p>
-        <p>
-          Likes: {parentComment.likeCount}
-          {
-            // Hide replies if clicked
-            comment.replies ?
-              <button onClick={() => {
-                let repliesContainer = document.getElementById(comment.snippet.topLevelComment.id)
-
-                repliesContainer.style.display == "block" ?
-                  repliesContainer.style.display = "none"
-                :
-                  repliesContainer.style.display = "block"
-              }}>
-                {comment.replies.comments.length} replies
-              </button>
-            : ""
-          }
-        </p>
-        <div id={comment.snippet.topLevelComment.id} style={{display: "none"}}>
-          {replies}
-        </div>
-      </div>
+  } else if (loading === false && comments.items.length === 0) { 
+    commentsJsx = (
+      <div>No Results</div>
     );
-  });
-
-
-  let menuJsx = <button type="button" onClick={() => setPrompt(true)}>Show Comments ({props.commentCount})</button>;
+  }
 
   if (prompt) {
     menuJsx = (
