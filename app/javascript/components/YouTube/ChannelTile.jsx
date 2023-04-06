66e37ff23a9e45f90093c272279c802587351287
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-04-05 22:47:45 -0700

    Changed render for ChannelTile depending if items array is available

--- app/javascript/components/YouTube/ChannelTile.jsx
+++ app/javascript/components/YouTube/ChannelTile.jsx
@@ -2,37 +2,68 @@ import React from "react";
 
 const ChannelTile = (props) => { 
   let c = props.channel;
-  let channel = c.items[0].snippet;
-  let stats = c.items[0].statistics;
-  let channelId = c.items[0].id;
   let addBtn = true;
-  // Changes button depending if user is subscribed to the channel.
-  for (let i = 0; i < props.addedChannels.length; i++) {
-    if (props.addedChannels[i]["channel_id"] == channelId) {
-      addBtn = false;
-    } 
+  let channelJsx = <div></div>;
+
+  if (c.items) {
+    let channel = c.items[0].snippet;
+    let stats = c.items[0].statistics;
+    let channelId = c.items[0].id;
+
+    // Changes button depending if user is subscribed to the channel.
+    for (let i = 0; i < props.addedChannels.length; i++) {
+      if (props.addedChannels[i]["channel_id"] == channelId) {
+        addBtn = false;
+      } 
+    }
+
+    channelJsx = (
+      <div key={channelId}>
+        <a href={`https://www.youtube.com/${channel.customUrl}`} target="_blank">
+          <img src={channel.thumbnails.default.url} alt="channel avatar" />
+          <span>{channel.title}</span>
+        </a>
+        <p>Description: {channel.description}</p>
+        <p>Subscribers count: {stats.subscriberCount}</p>
+        <p>Video Count: {stats.videoCount}</p>
+        <p>Total View Count: {stats.viewCount}</p>
+        <p>Joined: {channel.publishedAt}</p>
+
+        {
+          addBtn ? 
+            <button onClick={(e) => props.addShorts(e, channelId)}>Add</button>
+          : 
+            <button onClick={(e) => props.removeShorts(e, channelId)}>Remove</button>
+        }
+      </div>
+    );
+  } else {
+    // Changes button depending if user is subscribed to the channel.
+    for (let i = 0; i < props.addedChannels.length; i++) {
+      if (props.addedChannels[i]["channel_id"] == c.channelId) {
+        addBtn = false;
+      } 
+    }
+    // YouTube Search data does not include stats, a differnt JSX render is needed.
+    channelJsx = (
+      <div key={c.channelId}>
+        <img src={c.thumbnails.default.url} alt="channel avatar" />
+        <span>{c.title}</span>
+
+        <p>Description: {c.description}</p>
+        <p>Joined: {c.publishedAt}</p>
+
+        {
+          addBtn ? 
+            <button onClick={(e) => props.addShorts(e, c.channelId)}>Add</button>
+          : 
+            <button onClick={(e) => props.removeShorts(e, c.channelId)}>Remove</button>
+        }
+      </div>
+    );
   }
 
-  return (
-    <div key={channelId}>
-      <a href={`https://www.youtube.com/${channel.customUrl}`} target="_blank">
-        <img src={channel.thumbnails.default.url} alt="channel avatar" />
-        <span>{channel.title}</span>
-      </a>
-      <p>Description: {channel.description}</p>
-      <p>Subscribers count: {stats.subscriberCount}</p>
-      <p>Video Count: {stats.videoCount}</p>
-      <p>Total View Count: {stats.viewCount}</p>
-      <p>Joined: {channel.publishedAt}</p>
-
-      {
-        addBtn ? 
-          <button onClick={(e) => props.addShorts(e, channelId)}>Add</button>
-        : 
-          <button onClick={(e) => props.removeShorts(e, channelId)}>Remove</button>
-      }
-    </div>
-  );
+  return channelJsx;
 }
 
 export default ChannelTile;
