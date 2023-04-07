f2a22876bc3ac34584cf9e63ce737d6beecbda1d
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-04-06 22:52:03 -0700

    Conditionally loaded user index page

--- app/javascript/components/User/UserIndex.jsx
+++ app/javascript/components/User/UserIndex.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 const UserIndex = () => { 
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch("/api/v1/users/index")
@@ -12,18 +13,28 @@ const UserIndex = () => {
         }
         throw new Error("Network response was not ok.");
       })
-      .then((response) => setUsers(response))
+      .then((response) => setUsers(response), setLoading(false))
       .catch(() => console.log("Error getting users index"));
   }, []);
 
   let usersJsx = users.map(user => <div key={user.id}><Link to={`/users/show/${user.id}`}>{`${user.first_name} ${user.last_name}`}</Link></div>);
 
-  return (
-    <div>
-      <h1>User Index</h1>
-      {usersJsx}
-    </div>
-  );
+  let mainJsx = <h1>...Loading</h1>;
+
+  if (loading === false && users.length > 0) {
+    mainJsx = (
+      <div>
+        <h1>User Index</h1>
+        {usersJsx}
+      </div>
+    );
+  } else if (loading === false && users.length === 0) { 
+    mainJsx = (
+      <div>No Results</div>
+    );
+  }
+
+  return mainJsx;
 }
 
 export default UserIndex;
