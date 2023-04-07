5291faefc7fefb03ee05b420e41e5eec9f57c4f0
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-04-06 22:06:51 -0700

    Added callback to delete unrelated playlist items

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,9 +1,25 @@
 class Playlist < ApplicationRecord
   belongs_to :user
   has_many :playlist_videos, dependent: :destroy
+  before_update :destroy_unrelated_sources
   
   validates :name, :source, :user_id, presence: true
   validates :private, inclusion: { in: [ true, false ] }
   validates :source, inclusion: { in: [ "youtube", "reddit", "mix" ] }
   validates :name, length: {minimum: 1, maximum: 50}
+
+  # When playlist source is updated, delete unrelated videos
+  def destroy_unrelated_sources
+    source_is = self.source
+
+    if source_is != "mix" && source_changed? 
+      if source_was == 'mix' && source_is == 'reddit'
+        PlaylistVideo.where(playlist_id: self.id, source: 'youtube').destroy_all
+      elsif source_was == 'mix' && source_is == 'youtube'
+        PlaylistVideo.where(playlist_id: self.id, source: 'reddit').destroy_all
+      elsif source_is == 'reddit' || source_is == 'youtube' 
+        PlaylistVideo.where(playlist_id: self.id, source: source_was).destroy_all
+      end
+    end
+  end
 end
