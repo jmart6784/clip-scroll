149ce33c4b6e242db47ad8cc3fb8d7ab5943775c
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-04-07 22:49:12 -0700

    Added main logic to playlist video delete component

--- app/javascript/components/Playlist_video/PlaylistVideoDelete.jsx
+++ app/javascript/components/Playlist_video/PlaylistVideoDelete.jsx
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
-const PlaylistVideoDelete = () => { 
+const PlaylistVideoDelete = (props) => {
+  const [prompt, setPrompt] = useState(false);
+
+  const deletePlaylistVideo = () => { 
+    let pv = props.video;
+    let form = {
+      ['video_id']: pv['video_id'],
+      ['playlist_id']: pv['playlist_id'],
+      ['source']: pv['source'],
+      ['parent_source_id']: pv['parent_source_id'],
+    };
+
+    fetch('/api/v1/playlist_videos/destroy_from_playlist', {
+      method: "DELETE",
+      headers: {
+        "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]').content,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(form),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then((response) => props.removeVideo(pv.id))
+      .catch((error) => console.log(error.message));
+  }
+
+  let delJsx = <button type="button" onClick={() => setPrompt(true)}>Delete</button>;
+
+  if (prompt) {
+    delJsx = <div>
+      <p>Are you sure you would like to delete this playlist?</p>
+      <button onClick={deletePlaylistVideo} type="button">Delete</button>
+      <button onClick={() => setPrompt(false)} type="button">Cancel</button>
+    </div>
+  }
+
   return (
     <div>
-      <p>Delete Component</p>
+      {delJsx}
     </div>
   );
 }
