ef13504b8b59d3740da16a1864e7aa4fa1204282
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-04-09 17:38:14 -0700

    Added user association

--- app/models/user_configuration.rb
+++ app/models/user_configuration.rb
@@ -1,4 +1,6 @@
 class UserConfiguration < ApplicationRecord
+  belongs_to :user
+  
   validates :youtube_search_limit, :last_requested, :screen_fit, presence: true
   validates :screen_fit, inclusion: { in: [ "full", "zoom" ] }
   validates :user_id, uniqueness: true, presence: true
