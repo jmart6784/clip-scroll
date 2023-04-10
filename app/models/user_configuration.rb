56cef1b04d7b8bd026e975db5b7144de05aa3333
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-04-09 17:49:10 -0700

    Removed uneeded validation

--- app/models/user_configuration.rb
+++ app/models/user_configuration.rb
@@ -1,12 +1,7 @@
 class UserConfiguration < ApplicationRecord
   belongs_to :user
-  
+
   validates :youtube_search_limit, :last_requested, :screen_fit, presence: true
   validates :screen_fit, inclusion: { in: [ "full", "zoom" ] }
   validates :user_id, uniqueness: true, presence: true
-  validate :valid_datetime
-  
-  def valid_datetime
-    errors.add(:last_requested, 'must be a valid datetime') if ((DateTime.parse(last_requested) rescue ArgumentError) == ArgumentError)
-  end
 end
