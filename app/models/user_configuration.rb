7cefd0fa0ae8991241fa9fbaa51f3e473bb1b463
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-04-09 18:10:28 -0700

    Removed last_requested column out of presence validation

--- app/models/user_configuration.rb
+++ app/models/user_configuration.rb
@@ -1,7 +1,7 @@
 class UserConfiguration < ApplicationRecord
   belongs_to :user
 
-  validates :youtube_search_limit, :last_requested, :screen_fit, presence: true
+  validates :youtube_search_limit, :screen_fit, presence: true
   validates :screen_fit, inclusion: { in: [ "full", "zoom" ] }
   validates :user_id, uniqueness: true, presence: true
 end
