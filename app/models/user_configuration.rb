43e705a5e376e6a028e610d96b0116b762253db7
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-04-09 17:37:25 -0700

    added user configuration model validations

--- app/models/user_configuration.rb
+++ app/models/user_configuration.rb
@@ -1,2 +1,10 @@
 class UserConfiguration < ApplicationRecord
+  validates :youtube_search_limit, :last_requested, :screen_fit, presence: true
+  validates :screen_fit, inclusion: { in: [ "full", "zoom" ] }
+  validates :user_id, uniqueness: true, presence: true
+  validate :valid_datetime
+  
+  def valid_datetime
+    errors.add(:last_requested, 'must be a valid datetime') if ((DateTime.parse(last_requested) rescue ArgumentError) == ArgumentError)
+  end
 end
