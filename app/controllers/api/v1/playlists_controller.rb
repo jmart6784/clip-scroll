14cf741d178076dc37b4ad932e300289f350f477
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-04-10 22:13:29 -0700

    Added playlist show action to set_playlist check

--- app/controllers/api/v1/playlists_controller.rb
+++ app/controllers/api/v1/playlists_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::PlaylistsController < ApplicationController
-  before_action :set_playlist, only: [:update, :destroy]
+  before_action :set_playlist, only: [:show, :update, :destroy]
 
   def index
     playlist = Playlist.where(private: false).or(Playlist.where(user_id: current_user.id)).order(created_at: :desc)
