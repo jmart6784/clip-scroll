66259145ed0d0d9e8cb3bdbac6bcfd1f5446ee96
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-04-10 19:36:01 -0700

    Added image to home page

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ShortsImage from 'images/shorts.svg'
 
 const Home = () => {
   return (
     <div>
+      <img src={ShortsImage} alt="shorts logo" height="100" width="300" />
       <h1>Home</h1> <br />
       <Link to="/youtube/index">YouTube Index</Link> <br />
       <Link to="/reddit/index">Reddit Index</Link> <br />
