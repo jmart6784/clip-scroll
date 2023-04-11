18af2628c7e504ffcf51bce6952d8335f5cce2f0
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-04-10 19:42:05 -0700

    Added Reddit image

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import ShortsImage from 'images/shorts.svg'
+import RedditImage from 'images/Reddit.png'
 
 const Home = () => {
   return (
     <div>
       <img src={ShortsImage} alt="shorts logo" height="100" width="300" />
+      <img src={RedditImage} alt="shorts logo" height="100" width="300" />
       <h1>Home</h1> <br />
       <Link to="/youtube/index">YouTube Index</Link> <br />
       <Link to="/reddit/index">Reddit Index</Link> <br />
