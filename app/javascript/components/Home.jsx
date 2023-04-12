7d52da70e8c48e684fdaf23865056db5a2048287
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-04-11 20:29:06 -0700

    Styled home page

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -6,12 +6,15 @@ import RedditImage from 'images/Reddit.png'
 const Home = () => {
   return (
     <div>
-      <Link to="/youtube/index">
-        <img src={ShortsImage} alt="shorts logo" height="100" width="300" />
-      </Link>
-      <Link to="/reddit/index">
-        <img src={RedditImage} alt="shorts logo" height="100" width="300" />
-      </Link>
+      <div className="home-logo-wrapper">
+        <h1>What do you want to watch?</h1>
+        <Link to="/youtube/index" className="home-logo">
+          <img src={ShortsImage} alt="shorts logo" height="100" width="300" />
+        </Link>
+        <Link to="/reddit/index" className="home-logo">
+          <img src={RedditImage} alt="shorts logo" height="100" width="300" />
+        </Link>
+      </div>
     </div>
   );
 };
