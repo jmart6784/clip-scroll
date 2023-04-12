904a336c8748c035dc1ae2a04cf05f77f45950e5
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-04-11 21:53:26 -0700

    Implemented playlist card into playlist index

---
+++ app/javascript/components/Playlist/PlaylistCard.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import PlaylistDelete from "./PlaylistDelete";
+
+
+const PlaylistCard = (props) => { 
+  let user = props.playlist.user;
+  let playlist = props.playlist;
+  let ownerBtns = '';
+
+  if (props.user) {
+    if (user.id === props.user.id) {
+      ownerBtns = (
+        <div>
+          <Link to={`/playlist/edit/${playlist.id}`}>Edit</Link>
+          <PlaylistDelete id={playlist.id} location="index" />
+        </div>
+      );
+    }
+  }
+
+  return (
+    <div id={`playlist-${playlist.id}`} key={playlist.id}>
+      <strong><Link to={`/playlist/show/${playlist.id}`}>{playlist.name}</Link></strong>
+
+      {ownerBtns}
+      
+      <Link to={`/users/show/${user.id}`}>
+        <img src={user.avatar.url} height="50" width="50" alt="user avatar" />
+        <span>{user.username}</span>
+      </Link>
+    </div>
+  );
+}
+
+export default PlaylistCard;
--- app/javascript/components/Playlist/PlaylistIndex.jsx
+++ app/javascript/components/Playlist/PlaylistIndex.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import PlaylistDelete from "./PlaylistDelete";
 import GlobalContext from "../context/GlobalContext";
+import PlaylistCard from "./PlaylistCard";
 
 const PlaylistIndex = () => { 
   const [playlists, setPlaylists] = useState([]);
@@ -20,34 +21,7 @@ const PlaylistIndex = () => {
       .catch(() => console.log("Error getting data"));
   }, []);
 
-  let playListJsx = playlists.map(playlist => { 
-    let user = playlist.user;
-    let ownerBtns = '';
-
-    if (global.user) {
-      if (user.id === global.user.id) {
-        ownerBtns = (
-          <div>
-            <Link to={`/playlist/edit/${playlist.id}`}>Edit</Link>
-            <PlaylistDelete id={playlist.id} location="index" />
-          </div>
-        );
-      }
-    }
-
-    return (
-      <div id={`playlist-${playlist.id}`} key={playlist.id}>
-        <strong><Link to={`/playlist/show/${playlist.id}`}>{playlist.name}</Link></strong>
-
-        {ownerBtns}
-        
-        <Link to={`/users/show/${user.id}`}>
-          <img src={user.avatar.url} height="50" width="50" alt="user avatar" />
-          <span>{user.username}</span>
-        </Link>
-      </div>
-    );
-  });
+  let playListJsx = playlists.map(playlist => <PlaylistCard key={playlist.id} playlist={playlist} user={global.user} />);
 
   let mainJsx = <h1>...Loading</h1>;
 
