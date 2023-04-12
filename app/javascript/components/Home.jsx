a76478ff366feb8aa20498c8bbe856eb09049d3c
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-04-11 20:44:25 -0700

    Made Home page responsive

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div>
       <div className="home-logo-wrapper">
-        <h1>What do you want to watch?</h1>
+        <h3 className="home-title">What do you want to watch?</h3>
         <Link to="/youtube/index" className="home-logo">
           <img src={ShortsImage} alt="shorts logo" height="100" width="300" />
         </Link>
