d5170915717c3c87575a4ea87095f44336771f7c
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-04-12 23:47:13 -0700

    Added styling to Profile menu component

--- app/javascript/components/layouts/ProfileMenu.jsx
+++ app/javascript/components/layouts/ProfileMenu.jsx
@@ -2,11 +2,27 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const ProfileMenu = (props) => { 
+  let userId = props.user ? props.user.id : '';
+
   return (
-    <div>
-      <Link to={`/users/show/${props.user.id}`} onClick={() => props.hideMenu()}>Show Profile</Link>
-      <a href="/users/edit" onClick={() => props.hideMenu()}>Edit profile</a>
-      <a rel="nofollow" data-method="delete" href="/users/sign_out" onClick={() => props.hideMenu()}>Log out</a>
+    <div className="menu-div">
+      <div className="menu-link-wrapper">
+        <button className="close-nav-menu" type="button" onClick={() => props.hideMenu()}>
+          <i className="fa-sharp fa-solid fa-xmark"></i>
+        </button>
+
+        <Link to={`/users/show/${userId}`} onClick={() => props.hideMenu()} className="menu-link">
+          <i className="fa-solid fa-face-grin"></i> <span>My Profile</span>
+        </Link>
+
+        <a href="/users/edit" onClick={() => props.hideMenu()} className="menu-link">
+          <i className="fa-solid fa-user-pen"></i> <span>Edit profile</span>
+        </a>
+
+        <a rel="nofollow" data-method="delete" href="/users/sign_out" onClick={() => props.hideMenu()} className="menu-link">
+          <i className="fa-solid fa-power-off"></i> <span>Log out</span>
+        </a>
+      </div>
     </div>
   );
 }
