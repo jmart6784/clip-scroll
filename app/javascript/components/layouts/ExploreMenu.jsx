1813383b7f244e83efe939327c4560aaffff448a
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-04-12 22:38:04 -0700

    Added and styled closed button

--- app/javascript/components/layouts/ExploreMenu.jsx
+++ app/javascript/components/layouts/ExploreMenu.jsx
@@ -5,6 +5,10 @@ const ExploreMenu = (props) => {
   return (
     <div className="menu-div">
       <div className="menu-link-wrapper">
+        <button className="close-nav-menu" type="button">
+          <i className="fa-sharp fa-solid fa-xmark"></i>
+        </button>
+
         <Link to="/playlist/index" onClick={() => props.hideMenu()} className="menu-link">
           Playlist Index
         </Link>
