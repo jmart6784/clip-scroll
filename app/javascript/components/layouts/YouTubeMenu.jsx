031166a4e0b4aa901274ff9e434c0ac0db73654c
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-04-12 23:58:02 -0700

    Styled YouTube menu location

--- app/javascript/components/layouts/YouTubeMenu.jsx
+++ app/javascript/components/layouts/YouTubeMenu.jsx
@@ -3,10 +3,24 @@ import { Link } from "react-router-dom";
 
 const YouTubeMenu = (props) => { 
   return (
-    <div>
-      <Link to="/youtube/channels" onClick={() => props.hideMenu()}>Channels</Link>
-      <Link to="/youtube/my_channels" onClick={() => props.hideMenu()}>My Channels</Link>
-      <Link to="/youtube/index" onClick={() => props.hideMenu()}>YouTube Index</Link>
+    <div className="menu-div">
+      <div className="menu-link-wrapper">
+        <button className="close-nav-menu" type="button" onClick={() => props.hideMenu()}>
+          <i className="fa-sharp fa-solid fa-xmark"></i>
+        </button>
+
+        <Link to="/youtube/index" onClick={() => props.hideMenu()} className="menu-link">
+          <i className="fa-brands fa-youtube"></i> <span>YouTube Shorts</span>
+        </Link>
+
+        <Link to="/youtube/my_channels" onClick={() => props.hideMenu()} className="menu-link">
+          <i className="fa-solid fa-face-grin"></i> <span>My Channels</span>
+        </Link>
+
+        <Link to="/youtube/channels" onClick={() => props.hideMenu()} className="menu-link">
+          <i className="fa-solid fa-tv"></i> <span>Channels</span>
+        </Link>
+      </div>
     </div>
   );
 }
