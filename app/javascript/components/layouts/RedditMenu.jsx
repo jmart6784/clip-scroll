163e0436b82e4ab05b1c2f796961113d7655b413
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-04-13 22:15:33 -0700

    Styled reddit menu

--- app/javascript/components/layouts/RedditMenu.jsx
+++ app/javascript/components/layouts/RedditMenu.jsx
@@ -3,10 +3,24 @@ import { Link } from "react-router-dom";
 
 const RedditMenu = (props) => { 
   return (
-    <div>
-      <Link to="/subreddit/index" onClick={() => props.hideMenu()}>Subreddit Index</Link>
-      <Link to='/mysubreddits' onClick={() => props.hideMenu()}>My Subreddits</Link>
-      <Link to="/reddit/index" onClick={() => props.hideMenu()}>RedditIndex</Link>
+    <div className="menu-div">
+      <div className="menu-link-wrapper">
+        <button className="close-nav-menu" type="button" onClick={() => props.hideMenu()}>
+          <i className="fa-sharp fa-solid fa-xmark"></i>
+        </button>
+
+        <Link to="/reddit/index" onClick={() => props.hideMenu()} className="menu-link">
+          <i className="fa-brands fa-reddit-alien"></i> <span>Reddit Videos</span>
+        </Link>
+
+        <Link to='/mysubreddits' onClick={() => props.hideMenu()} className="menu-link">
+          <i className="fa-solid fa-face-grin"></i> <span>My Subreddits</span>
+        </Link>
+
+        <Link to="/subreddit/index" onClick={() => props.hideMenu()} className="menu-link">
+          <i className="fa-brands fa-reddit"></i> <span>Subreddits</span>
+        </Link>
+      </div>
     </div>
   );
 }
