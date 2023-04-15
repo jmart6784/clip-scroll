ebe4a1a6f6e148c9eae0594aff55316226c8e3ed
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-04-14 22:57:19 -0700

    Added validation to youtube channel refresh limit column

--- app/models/user_configuration.rb
+++ app/models/user_configuration.rb
@@ -1,7 +1,7 @@
 class UserConfiguration < ApplicationRecord
   belongs_to :user
 
-  validates :youtube_search_limit, :screen_fit, presence: true
+  validates :youtube_search_limit, :screen_fit, :youtube_channel_refresh_limit, presence: true
   validates :screen_fit, inclusion: { in: [ "full", "zoom" ] }
   validates :user_id, uniqueness: true, presence: true
 end
