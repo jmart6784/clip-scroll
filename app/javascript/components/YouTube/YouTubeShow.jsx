aa4ffb01fb77d4a60c653314094086046e65764d
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-04-15 21:15:09 -0700

    Added Frontend request to YouTubeShow component

--- app/javascript/components/YouTube/YouTubeShow.jsx
+++ app/javascript/components/YouTube/YouTubeShow.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const YouTubeShow = (props) => { 
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => { 
+    fetch(`/api/v1/youtube/videos_by_channel/${props.id}`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then((response) => setvideos(response))
+      .catch(() => console.log("Error getting data"));
+  }, []);
+
+  useEffect(() => console.log(videos), [videos]);
+
   return (
     <div>
       <h1>YouTube Show</h1>
