8f061497a8dd74a319c2fc620f2bd6e2041a2c43
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-04-15 21:21:46 -0700

    adjusted props ID

--- app/javascript/components/YouTube/YouTubeShow.jsx
+++ app/javascript/components/YouTube/YouTubeShow.jsx
@@ -4,15 +4,15 @@ const YouTubeShow = (props) => {
   const [videos, setVideos] = useState([]);
 
   useEffect(() => { 
-    fetch(`/api/v1/youtube/videos_by_channel/${props.id}`)
+    fetch(`/api/v1/youtube/videos_by_channel/${props.match.params.id}`)
       .then((response) => {
         if (response.ok) {
           return response.json();
         }
         throw new Error("Network response was not ok.");
       })
-      .then((response) => setvideos(response))
-      .catch(() => console.log("Error getting data"));
+      .then((response) => setVideos(response))
+      .catch(() => console.log("Error getting video data"));
   }, []);
 
   useEffect(() => console.log(videos), [videos]);
