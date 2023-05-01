986b61490d1582f5fa179c18140796c3ede52f1c
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-04-30 20:45:41 -0700

    Added icons

--- app/javascript/components/YouTube/ChannelsNav.jsx
+++ app/javascript/components/YouTube/ChannelsNav.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const ChannelsNav = (props) => {
-  let channels = <div>Channels</div>;
-  let search = <div>Search</div>;
-  let myChannels = <div>My Channels</div>;
+  let channels = <div><i className="fa-brands fa-youtube"></i> Channels</div>;
+  let search = <div><i className="fa-solid fa-magnifying-glass"></i> Search</div>;
+  let myChannels = <div><i className="fa-solid fa-face-grin"></i> My Channels</div>;
 
   if (props.view == 'channels') {
-    search = <Link to="/youtube/search">Search</Link>;
-    myChannels = <Link to="/youtube/my_channels">My Channels</Link>;
+    search = <Link to="/youtube/search"><i className="fa-solid fa-magnifying-glass"></i> Search</Link>;
+    myChannels = <Link to="/youtube/my_channels"><i className="fa-solid fa-face-grin"></i> My Channels</Link>;
   } else if (props.view == 'search') { 
-    channels = <Link to="/youtube/channels">Channels</Link>;
-    myChannels = <Link to="/youtube/my_channels">My Channels</Link>;
+    channels = <Link to="/youtube/channels"><i className="fa-brands fa-youtube"></i> Channels</Link>;
+    myChannels = <Link to="/youtube/my_channels"><i className="fa-solid fa-face-grin"></i> My Channels</Link>;
   } else if (props.view == 'my channels') {
-    channels = <Link to="/youtube/channels">Channels</Link>;
-    search = <Link to="/youtube/search">Search</Link>;
+    channels = <Link to="/youtube/channels"><i className="fa-brands fa-youtube"></i> Channels</Link>;
+    search = <Link to="/youtube/search"><i className="fa-solid fa-magnifying-glass"></i> Search</Link>;
   }
 
   return (
