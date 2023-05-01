3b0a6302a5aeb8022d6bd34e707e0396956ac679
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-04-30 19:32:05 -0700

    Added class name to no channels div

--- app/javascript/components/YouTube/NoChannels.jsx
+++ app/javascript/components/YouTube/NoChannels.jsx
@@ -4,7 +4,7 @@ import ytImage from 'images/youtube-error.png'
 
 const NoChannels = () => { 
   return (
-    <div>
+    <div className="no-channels-div">
       <img src={ytImage} alt="youtube error" />
       <div>
         No Channels added, search for more <Link to="/youtube/search">here!</Link>
