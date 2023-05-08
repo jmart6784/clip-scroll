62d3d48d2004b937a00e9727e259f733a049756d
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-05-07 21:26:39 -0700

    Added loading gif to general loading component

---
+++ app/javascript/helpers/GeneralLoading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Loading from "images/loading-gif.gif";
+
+const GeneralLoading = () => { 
+  return (
+    <div>
+      <img src={Loading} alt="loading icon" />
+    </div>
+  );
+}
+
+export default GeneralLoading;
