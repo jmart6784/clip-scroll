9b1d2e6693cac8e76fe47115b0d32954e3654c7e
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-05-07 21:32:45 -0700

    Styled general loading component

--- app/javascript/helpers/GeneralLoading.jsx
+++ app/javascript/helpers/GeneralLoading.jsx
@@ -3,7 +3,7 @@ import Loading from "images/loading-gif.gif";
 
 const GeneralLoading = () => { 
   return (
-    <div>
+    <div className="general-loading">
       <img src={Loading} alt="loading icon" />
     </div>
   );
