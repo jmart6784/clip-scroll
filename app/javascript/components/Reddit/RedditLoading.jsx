f951cdff27a49314b32574b9175f049776b0be6d
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-05-13 23:22:12 -0700

    Renamed reddit nav

---
+++ app/javascript/components/Reddit/RedditLoading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import loading from "images/reddit-loading.gif";
+
+const RedditLoading = () => { 
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default RedditLoading;
