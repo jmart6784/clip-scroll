a7c3b1043bdbb5adf9a0ac08457934ab20b70cd4
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-05-14 13:31:41 -0700

    Added RedditNav logic

--- app/javascript/components/Reddit/RedditNav.jsx
+++ app/javascript/components/Reddit/RedditNav.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const RedditNav = () => { 
-  return (
-    <div>
+  let subreddits = <div><i className="fa-brands fa-youtube"></i> Subreddits</div>;
+  let search = <div><i className="fa-solid fa-magnifying-glass"></i> Search</div>;
+  let mySubreddits = <div><i className="fa-solid fa-face-grin"></i> My Subreddits</div>;
 
+  if (props.view == 'subreddits') {
+    search = <Link to='/reddit/search'><i className="fa-solid fa-magnifying-glass"></i> Search</Link>;
+    mySubreddits = <Link to='/mysubreddits'><i className="fa-solid fa-face-grin"></i> My Subreddits</Link>;
+  } else if (props.view == 'search') {
+    subreddits = <Link to='/subreddit/index'><i className="fa-brands fa-reddit-alien"></i> Subreddit</Link>;
+    mySubreddits = <Link to='/mysubreddits'><i className="fa-solid fa-face-grin"></i> My Subreddits</Link>;
+  } else if (props.view == 'my subreddits') { 
+    subreddits = <Link to='/subreddit/index'><i className="fa-brands fa-reddit-alien"></i> Subreddit</Link>;
+    search = <Link to='/reddit/search'><i className="fa-solid fa-magnifying-glass"></i> Search</Link>;
+  }
+
+  return (
+    <div className="channels-nav-wrapper">
+      {subreddits}
+      {search}
+      {mySubreddits}
     </div>
   );
 }
