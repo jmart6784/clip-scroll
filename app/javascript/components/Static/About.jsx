68131895da353c8aba985510fd375765f2428287
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-06-13 21:21:12 -0700

    Added to about section

--- app/javascript/components/Static/About.jsx
+++ app/javascript/components/Static/About.jsx
@@ -6,7 +6,9 @@ const About = () => {
       <h1>About</h1>
 
       <div>
-        <p>This web application utilizes both the Reddit and Youtube API to display and serve vertical videos.</p>
+        <p>
+          This web application utilizes both the Reddit and Youtube API to display and serve vertical videos. You can create custom playlists that support both platforms.
+        </p>
       </div>
     </div>
   );
