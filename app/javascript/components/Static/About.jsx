3b727326dc2b5661c88dbfdb26fe15b93c861722
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-06-22 23:52:29 -0700

    styled reddit logo

--- app/javascript/components/Static/About.jsx
+++ app/javascript/components/Static/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import youtubelogo from "images/youtube-logo.png";
+import redditlogo from "images/reddit-logo.png";
 
 const About = () => { 
   return (
@@ -16,6 +17,8 @@ const About = () => {
         <p>
           The Youtube content that is served in this web application utilizes the YouTube v3 API. This API allows for 10,000 units of API calls per day. This is a good amount of requests, but I have limited some feature like the channels search and channel refreshes. As it consumes more request units. Features such as liking and creating comments are not currently implemented into this application.
         </p>
+
+        <img src={redditlogo} alt="youtube logo" className="ab-reddit-logo" />
       </div>
     </div>
   );
