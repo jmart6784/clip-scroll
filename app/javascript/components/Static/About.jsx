b432c30d050f91d4cfa9cec78958ff36548f674f
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-06-23 22:16:43 -0700

    Added reddit about section to about page

--- app/javascript/components/Static/About.jsx
+++ app/javascript/components/Static/About.jsx
@@ -15,10 +15,14 @@ const About = () => {
         <img src={youtubelogo} alt="youtube logo" className="ab-yt-logo" />
 
         <p>
-          The Youtube content that is served in this web application utilizes the YouTube v3 API. This API allows for 10,000 units of API calls per day. This is a good amount of requests, but I have limited some feature like the channels search and channel refreshes. As it consumes more request units. Features such as liking and creating comments are not currently implemented into this application.
+          The Youtube content that is served in this web application utilizes the YouTube v3 API. This API allows for 10,000 units of API calls per day. This is a good amount of requests, but I have limited some features like the channels search and channel refreshes. As it consumes more request units. Features such as liking and creating comments are not currently implemented into this application.
         </p>
 
         <img src={redditlogo} alt="youtube logo" className="ab-reddit-logo" />
+
+        <p>
+          The Reddit video content that is served is based on the Reddit API, it does not utilize an OAuth client ID. When I began creating this project the free tier query limit was 60 per minute. As of July 1, 2023 the limit will be decreased down to 10 queries per minute. This change significantly limits the functionality of the application. As for unsupported features it is similar to the YouTube content. Upvoting, commenting and awarding are currently unsupported.
+        </p>
       </div>
     </div>
   );
