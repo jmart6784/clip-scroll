e309eadaf3508b30d34100b470eff080d62fe06c
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-06-24 19:55:08 -0700

    Added Javascript logic to hide flash messages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,3 +10,29 @@ import "@fortawesome/fontawesome-free/css/all.css";
 
 Rails.start();
 ActiveStorage.start();
+
+// Hide flash messages after 3 seconds
+// let generalInterval = setInterval(() => {
+//   console.log("test interval");
+
+//   let notice = document.getElementsByClassName("notice")[0];
+//   let alert = document.getElementsByClassName("alert")[0];
+
+//   if (notice) {
+//     setTimeout(() => {
+//       notice.textContent === "" ? (notice.style.display = "none") : "";
+
+//       notice.style.display = "none";
+//     }, 3000);
+//   }
+
+//   if (alert) {
+//     alert.textContent === "" ? (alert.style.display = "none") : "";
+
+//     setTimeout(() => {
+//       alert.style.display = "none";
+//     }, 3000);
+//   }
+// }, 1000);
+
+// clearInterval(generalInterval);
