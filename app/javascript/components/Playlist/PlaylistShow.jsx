f2caff0b702ab2c11dab3824cad5d035c2c101c9
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-06-26 22:29:07 -0700

    Migrated playlist menu to PlaylistUI component

--- app/javascript/components/Playlist/PlaylistShow.jsx
+++ app/javascript/components/Playlist/PlaylistShow.jsx
@@ -1,12 +1,9 @@
 import React, { useEffect, useState, useContext } from "react";
-import { Link } from "react-router-dom";
-import PlaylistDelete from "./PlaylistDelete";
 import YoutubeVideo from "../YouTube/YouTubeVideo";
 import RedditVideo from "../Reddit/RedditVideo";
-import PlaylistVideoDelete from "../Playlist_video/PlaylistVideoDelete";
 import GlobalContext from "../context/GlobalContext";
-import PlayImage from 'images/play.png'
 import GeneralNoResults from "../../helpers/GeneralNoResults";
+import PlaylistUI from "./PlaylistUI";
 
 const PlaylistShow = (props) => {
   const [playlist, setPlaylist] = useState({
@@ -39,69 +36,6 @@ const PlaylistShow = (props) => {
       
     moreVideos();
   }, []);
-
-  // Select video by index when option is clicked on playlist menu
-  const selectVideo = (videoId) => {
-    let index = videos.findIndex(v => v.video_id === videoId);
-    index != -1 ? setIndex(index) : ""
-  }
-
-  const removeVideo = (videoId) => {
-    // If the last video is deleted, decrement the index
-    videos.length - 1 == index ? setIndex(index - 1) : '';
-    setVideos(videos.filter(vid => vid.id !== videoId));
-  }
-  
-  // Render click-able playlist menu options
-  let videosJsx = videos.map((v, i) => {
-    let selectedStyle = index == i ? { border: "1px solid black", cursor: "pointer" } : {};
-    let ownerBtns = '';
-
-    if (global.user) {
-      if (playlist['user_id'] === global.user.id) {
-        ownerBtns = <PlaylistVideoDelete video={v} removeVideo={removeVideo} />;
-      }
-    }
-    
-    if (v['source'] == 'youtube') {
-      let video = v["video"]["items"][0]["snippet"];
-      let videoId = v["video"]["items"][0]["id"];
-      let stats = v["video"]["items"][0]["statistics"];
-
-      return (
-        <div key={videoId} onClick={() => selectVideo(videoId)} style={selectedStyle}>
-          <img src={video.thumbnails.default.url} alt="video thumbnail" />
-          <strong>YouTube {v['video_id']}</strong>
-          <p>{video.title}</p>
-          <p>{video.channelTitle}</p>
-          <p>{stats.viewCount}</p>
-          {ownerBtns}
-        </div>
-      );
-    } else if (v['source'] == 'reddit') { 
-      let video = v['video']['data'];
-
-      return (
-        <div key={v['id']} onClick={() => selectVideo(video['id'])} style={selectedStyle}>
-          <img
-            src={video['thumbnail']}
-            alt="video thumbnail" 
-            onError={e => {
-              e.target.onerror = null;
-              e.target.src = PlayImage;
-            }}
-            height="100"
-            width="100"
-          />
-          <strong>Reddit: {v['video_id']}</strong>
-          <p>{video['title']}</p>
-          <p>{video['subreddit']}</p>
-          <p>{video['score']}</p>
-          {ownerBtns}
-        </div>
-      ); 
-    }
-  });
 
   const nextVideo = () => index != videos.length - 1 ? setIndex(index + 1) : moreVideos();
   const previousVideo = () => index > 0 ? setIndex(index - 1) : "";
@@ -162,41 +96,21 @@ const PlaylistShow = (props) => {
   let mainJsx = <h1>...Loading</h1>;
 
   if (loading === false && videos.length > 0) {
-    let ownerBtns = '';
-
-    if (global.user) {
-      if (playlist['user_id'] === global.user.id) {
-        ownerBtns = (
-          <div>
-            <Link to={`/playlist/edit/${playlist.id}`}>Edit</Link>
-            <PlaylistDelete id={playlist.id} />
-          </div>
-        );
-      }
-    }
-
     mainJsx = (
       <div>
-        {/* <p>name: {playlist.name}</p>
-        <p>Private: {playlist.private.toString()}</p>
-        <p>Source: {playlist.source}</p> */}
-        
-        {/* {ownerBtns} */}
-
-        {/* <div>
-          <p>{playlist.user.username}</p>
-          <Link to={`/users/show/${playlist.user.id}`}>
-            <img src={playlist.user.avatar.url} height="50" width="50" alt="user avatar" />
-            <span>{playlist.user.username}</span>
-          </Link>
-        </div> */}
-
         {/* <button onClick={previousVideo}>Previous</button>
         <button onClick={nextVideo} disabled={
           (noResults && videos.length == 0) || index == videos.length - 1
         }>Next</button> */}
-        {/* {videosJsx} */}
-        {/* <button onClick={moreVideos} disabled={noResults} id="more-btn">Show More</button> */}
+        <PlaylistUI
+          playlist={playlist}
+          videos={videos}
+          setIndex={setIndex}
+          setVideos={setVideos}
+          moreVideos={moreVideos}
+          noResults={noResults}
+          index={index}
+        />
         <div className="yt-video-listing-div">{vidJsx}</div>;
       </div>
     );
---
+++ app/javascript/components/Playlist/PlaylistUI.jsx
@@ -0,0 +1,112 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import PlayImage from 'images/play.png'
+import PlaylistVideoDelete from "../Playlist_video/PlaylistVideoDelete";
+import GlobalContext from "../context/GlobalContext";
+import PlaylistDelete from "./PlaylistDelete";
+
+const PlaylistUI = (props) => { 
+  const [global, setGlobal] = useContext(GlobalContext);
+
+  // Select video by index when option is clicked on playlist menu
+  const selectVideo = (videoId) => {
+    let index = props.videos.findIndex(v => v.video_id === videoId);
+    index != -1 ? props.setIndex(index) : ""
+  }
+
+  const removeVideo = (videoId) => {
+    // If the last video is deleted, decrement the index
+    props.videos.length - 1 == props.index ? props.setIndex(props.index - 1) : '';
+    props.setVideos(props.videos.filter(vid => vid.id !== videoId));
+  }
+
+  // Render click-able playlist menu options
+  let videosJsx = props.videos.map((v, i) => {
+    let selectedStyle = props.index == i ? { border: "1px solid black", cursor: "pointer" } : {};
+    let ownerBtns = '';
+
+    if (global.user) {
+      if (props.playlist['user_id'] === global.user.id) {
+        ownerBtns = <PlaylistVideoDelete video={v} removeVideo={removeVideo} />;
+      }
+    }
+    
+    if (v['source'] == 'youtube') {
+      let video = v["video"]["items"][0]["snippet"];
+      let videoId = v["video"]["items"][0]["id"];
+      let stats = v["video"]["items"][0]["statistics"];
+
+      return (
+        <div key={videoId} onClick={() => selectVideo(videoId)} style={selectedStyle}>
+          <img src={video.thumbnails.default.url} alt="video thumbnail" />
+          <strong>YouTube {v['video_id']}</strong>
+          <p>{video.title}</p>
+          <p>{video.channelTitle}</p>
+          <p>{stats.viewCount}</p>
+          {ownerBtns}
+        </div>
+      );
+    } else if (v['source'] == 'reddit') { 
+      let video = v['video']['data'];
+
+      return (
+        <div key={v['id']} onClick={() => selectVideo(video['id'])} style={selectedStyle}>
+          <img
+            src={video['thumbnail']}
+            alt="video thumbnail" 
+            onError={e => {
+              e.target.onerror = null;
+              e.target.src = PlayImage;
+            }}
+            height="100"
+            width="100"
+          />
+          <strong>Reddit: {v['video_id']}</strong>
+          <p>{video['title']}</p>
+          <p>{video['subreddit']}</p>
+          <p>{video['score']}</p>
+          {ownerBtns}
+        </div>
+      ); 
+    }
+  });
+
+  let ownerBtns = '';
+
+  if (global.user) {
+    if (props.playlist['user_id'] === global.user.id) {
+      ownerBtns = (
+        <div>
+          <Link to={`/playlist/edit/${props.playlist.id}`}>Edit</Link>
+          <PlaylistDelete id={props.playlist.id} />
+        </div>
+      );
+    }
+  }
+
+  return (
+    <div>
+      <div>
+        <p>name: {props.playlist.name}</p>
+        <p>Private: {props.playlist.private.toString()}</p>
+        <p>Source: {props.playlist.source}</p>
+      </div>
+      
+      <div>
+          <p>{props.playlist.user.username}</p>
+          <Link to={`/users/show/${props.playlist.user.id}`}>
+            <img src={props.playlist.user.avatar.url} height="50" width="50" alt="user avatar" />
+            <span>{props.playlist.user.username}</span>
+          </Link>
+      </div>
+
+      {ownerBtns}
+      
+      {videosJsx}
+
+      <button onClick={props.moreVideos} disabled={props.noResults} id="more-btn">Show More</button>
+    </div>
+  );
+}
+
+export default PlaylistUI;
