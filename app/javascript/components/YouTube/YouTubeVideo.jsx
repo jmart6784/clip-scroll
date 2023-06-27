06f64955b6386c3678a1f4b79f3cbdee8b3de32e
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-06-26 19:46:53 -0700

    formatting

--- app/javascript/components/YouTube/YouTubeVideo.jsx
+++ app/javascript/components/YouTube/YouTubeVideo.jsx
@@ -5,13 +5,13 @@ import prettyNumbers from "../../helpers/prettyNumbers";
 import PlaylistVideoMenu from "../Playlist_video/PlaylistVideoMenu";
 import Draggable from "react-draggable";
 
-const YoutubeVideo = (props) => { 
+const YoutubeVideo = (props) => {
   const [video, setVideo] = useState({});
   const [commentsShow, setCommentsShow] = useState(false);
   const [playlistsShow, setPlaylistsShow] = useState(false);
   const [videoDrag, setVideoDrag] = useState(false);
   
-  useEffect(() => { 
+  useEffect(() => {
     // Example of deleted video /api/v1/youtube/video/xRa9SZUdk_Q
     fetch(`/api/v1/youtube/video/${props.id}`)
       .then((response) => {
@@ -24,7 +24,7 @@ const YoutubeVideo = (props) => {
       .catch(() => console.log("Failed to get video"));
   }, [props.id]);
 
-  const onReady = (event) =>  event.target.unMute();
+  const onReady = (event) => event.target.unMute();
   const onEnd = (event) => event.target.playVideo();
   const toggleComments = () => setCommentsShow(!commentsShow);
   const togglePlaylists = () => setPlaylistsShow(!playlistsShow);
@@ -37,7 +37,7 @@ const YoutubeVideo = (props) => {
   let statsJsx = '';
 
   if (video.items) {
-    if (video.items.length != 0) { 
+    if (video.items.length != 0) {
       videoId = video.items ? video.items[0].id : '';
       channelId = video.items ? video.items[0].snippet.channelId : '';
       stats = video.items ? video.items[0].statistics : { viewCount: '0', likeCount: '0', favoriteCount: '0', commentCount: '0' };
@@ -80,33 +80,33 @@ const YoutubeVideo = (props) => {
           {/* If comment or playlist menu is visible, icons */}
           {commentsShow || playlistsShow ? '' : (
             <>
-            <div className="yt-stat-div">
-              <i className="fa-solid fa-thumbs-up"></i>
-              <span>{prettyNumbers(stats.likeCount, 1)}</span>
-            </div>
+              <div className="yt-stat-div">
+                <i className="fa-solid fa-thumbs-up"></i>
+                <span>{prettyNumbers(stats.likeCount, 1)}</span>
+              </div>
 
-            <div className="yt-stat-div">
-              <i className="fa-solid fa-thumbs-down"></i> <span></span>
-            </div>
+              <div className="yt-stat-div">
+                <i className="fa-solid fa-thumbs-down"></i> <span></span>
+              </div>
 
-            <div className="yt-stat-div">
-              <i className="fa-solid fa-eye"></i>
-              <span>{prettyNumbers(stats.viewCount, 1)}</span>
-            </div>
+              <div className="yt-stat-div">
+                <i className="fa-solid fa-eye"></i>
+                <span>{prettyNumbers(stats.viewCount, 1)}</span>
+              </div>
             </>
           )}
           {/* If playlist menu is visible, hide comment menu */}
           {playlistsShow ? '' : cJsx}
           {/* If comment menu is visible, hide playlist menu */}
-          { 
+          {
             commentsShow ? '' : (
-            <PlaylistVideoMenu
-              videoId={videoId}
-              parentSourceId={channelId}
-              source="youtube"
-              togglePlaylists={togglePlaylists}  
-              toggleDrag={toggleDrag}
-            />
+              <PlaylistVideoMenu
+                videoId={videoId}
+                parentSourceId={channelId}
+                source="youtube"
+                togglePlaylists={togglePlaylists}
+                toggleDrag={toggleDrag}
+              />
             )
           }
         </div>
