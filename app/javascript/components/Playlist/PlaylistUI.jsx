4fb7fb6bd13c79ea599a76f6e2596d2607881c0e
jmart6784 <50716045+jmart6784@users.noreply.github.com>
2023-06-26 23:43:25 -0700

    Added close id  to close button

--- app/javascript/components/Playlist/PlaylistUI.jsx
+++ app/javascript/components/Playlist/PlaylistUI.jsx
@@ -90,7 +90,7 @@ const PlaylistUI = (props) => {
 
     menuJsx = (
       <div>
-        <i onClick={() => setPrompt(false)} class="fa-solid fa-xmark"></i>
+        <i id="pui-close" onClick={() => setPrompt(false)} class="fa-solid fa-xmark"></i>
         <div>
           <p>name: {props.playlist.name}</p>
           <p>Private: {props.playlist.private.toString()}</p>
